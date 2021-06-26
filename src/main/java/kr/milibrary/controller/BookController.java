bbed6d5bd88231947c53bd23923283f1223d4c0d
knight7024 <knight7024@naver.com>
2021-06-27 01:58:33 +0900

    @Valid를 import 하지 않던 문제 수정

--- src/main/java/kr/milibrary/controller/BookController.java
+++ src/main/java/kr/milibrary/controller/BookController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Auth
 @RequestMapping("/api")
 @RestController
